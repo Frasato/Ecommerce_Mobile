e755c0da60f7b1a2f552aa4df388a319caaa797e
Gabriel Frasato <gabriel.frasato@hotmail.com>
2025-05-30 16:48:38 -0300

    :sparkles: feat: Add Things on Home Page

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:ecommerce_app/widgets/custom_drawer_navigation.dart';
+import 'package:ecommerce_app/widgets/custom_text_field.dart';
+import 'package:ecommerce_app/widgets/header.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget{
@@ -19,8 +21,27 @@ class _HomePageState extends State<HomePage>{
         color: Colors.white
       ),
     ),
-    body: Column(
-      children: [],
-    ),
+    body: SingleChildScrollView(
+      scrollDirection: Axis.vertical,
+      child:Column(
+        children: [
+          Header(),
+          SizedBox(height: 50),
+          Container(
+            width: 300,
+            height: 150,
+            decoration: BoxDecoration(
+              color: Colors.amber,
+              borderRadius: BorderRadius.all(
+                Radius.circular(10)
+              )
+            ),
+            child: Text('Banner para promoções'),
+          ),
+          SizedBox(height: 20),
+          CustomTextField()
+        ],
+      ),
+    ) 
   );
 }
