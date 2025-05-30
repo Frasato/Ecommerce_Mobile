23caa8d2297038cf05bab37e669420533fb0474e
Gabriel Frasato <gabriel.frasato@hotmail.com>
2025-05-30 17:01:03 -0300

    :sparkles: feat: Create Constants Colors

---
+++ lib/constants/custom_colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const Color customBlue = Color.fromARGB(255, 35, 151, 223);
+const Color customYellow = Color.fromARGB(255, 254, 237, 0);
+const Color customRed = Color.fromARGB(255, 210, 14, 14);
+const Color customTextGrey = Color.fromARGB(255, 56, 56, 56);
+const Color customLightGrey = Color.fromARGB(255, 247, 247, 247);
