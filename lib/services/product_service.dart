76eb704525a105abe40b9c9aaab42790e88e978d
Gabriel Frasato <gabriel.frasato@hotmail.com>
2025-05-30 12:40:32 -0300

    :sparkles: feat: Request products service

--- lib/services/product_service.dart
+++ lib/services/product_service.dart
@@ -25,4 +25,60 @@ class ProductService {
     }
   }
 
+  Future<List<Map<String, dynamic>>> getProductByCategory(String category) async{
+    final prefs = await SharedPreferences.getInstance();
+    String? token = prefs.getString('token');
+
+    final response = await http.get(
+      Uri.parse('$apiUrl/product/cat/$category'),
+      headers: {
+        'Content-Type':'application/json',
+        'Authorization':'Bearer $token'
+      }
+    );
+
+    if(response.statusCode == 200){
+      return List<Map<String, dynamic>>.from(jsonDecode(response.body));
+    }else{
+      throw Exception('Falha ao carregar produtos');
+    }
+  }
+
+  Future<Map<String, dynamic>> getProductById(String productId) async{
+    final prefs = await SharedPreferences.getInstance();
+    String? token = prefs.getString('token');
+
+    final response = await http.get(
+      Uri.parse('$apiUrl/product/$productId'),
+      headers: {
+        'Content-Type':'application/json',
+        'Authorization':'Bearer $token'
+      },
+    );
+
+    if(response.statusCode == 200){
+      return Map<String, dynamic>.from(jsonDecode(response.body));
+    }else{
+      throw Exception('Falha ao carregar o produto');
+    }
+  }
+
+  Future<List<Map<String, dynamic>>> getPurchaseProduct() async{
+    final prefs = await SharedPreferences.getInstance();
+    String? token = prefs.getString('token');
+
+    final response = await http.get(
+      Uri.parse('$apiUrl/product/purchase'),
+      headers: {
+        'Content-Type':'application/json',
+        'Authorization':'Bearer $token'
+      },
+    );
+
+    if(response.statusCode == 200){
+      return List<Map<String, dynamic>>.from(jsonDecode(response.body));
+    }else{
+      throw Exception('Falha ao carregar o produtos');
+    }
+  }
 }
