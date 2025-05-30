5338d7f2f7b5c873b84be7316c5e59a89a7af4d5
Gabriel Frasato <gabriel.frasato@hotmail.com>
2025-05-30 11:33:32 -0300

    :sparkles: feat: Create Register and Login methods

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -1,3 +1,6 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
 class AuthService {
 
   static Future<bool> register(
@@ -15,7 +18,55 @@ class AuthService {
       return false;
     }
 
-    
+    final response = await http.post(
+      Uri.parse(''),
+      headers: {'Content-Type':'application/json'},
+      body: jsonEncode({
+        'name': name,
+        'password': password,
+        'email': email,
+        'cpf': cpf,
+        'street': street,
+        'number': number,
+        'city': city,
+        'CEP': cep
+      })
+    );
 
+    if(response.statusCode == 201){
+      return true;
+    }
+
+    return false;
+  }
+
+  static Future<String?> login(String email, String password) async{
+    if(email.isEmpty || password.isEmpty){
+      return null;
+    }
+
+    final response = await http.post(
+      Uri.parse(''),
+      headers: {'Content-Type':'application/json'},
+      body: jsonEncode({
+        'email': email,
+        'password': password
+      })
+    );
+
+    if(response.statusCode == 200){
+      final data = jsonDecode(response.body);
+      String userName = data['name'];
+      String token = data['token'];
+      String userId = data['userId'];
+
+      if(token == ''){
+        return null;
+      }
+
+      return token;
+    }
+
+    return null;
   }
 }
