0ae393ebfcca1b87594724f34343bcf55f4135ae
Gabriel Frasato <gabriel.frasato@hotmail.com>
2025-05-31 10:28:14 -0300

    :sparkles: feat: Add product card props

--- lib/widgets/product_card.dart
+++ lib/widgets/product_card.dart
@@ -2,50 +2,70 @@ import 'package:ecommerce_app/constants/custom_colors.dart';
 import 'package:flutter/material.dart';
 
 class ProductCard extends StatelessWidget{
-  const ProductCard({super.key});
+  final Map<String, dynamic> product;
+  final VoidCallback onTap;
+
+  const ProductCard({
+    super.key,
+    required this.product,
+    required this.onTap,
+  });
+
+  double price(){
+    double price;
+
+    if(product['price'] > product['priceWithDiscount'] && product['priceWithDiscount'] > 0){
+      int value = product['priceWithDiscount'];
+      price = value / 100;
+    }else{
+      int value = product['priceWithDiscount'];
+      price = value / 100;
+    }
+    
+    return price;
+  }
 
   @override
   Widget build(BuildContext context) => Padding(
     padding: const EdgeInsets.all(12),
-    child: Container(
-      width: 130,
-      height: 185,
-      decoration: BoxDecoration(
-        color: customLightGrey,
-        boxShadow: <BoxShadow>[
-          BoxShadow(
-            color: const Color.fromARGB(32, 56, 56, 56),
-            blurRadius: 13.0,
-            offset: Offset(10, 6)
-          )
-        ],
-        borderRadius: BorderRadius.circular(10)
-      ),
-      child: Padding(
-        padding: const EdgeInsets.all(5),
-        child: Column(
-          children: [
-            Image(
-              image: AssetImage('assets/icon.png'),
-              width: 50,
-              height: 50,
-            ),
-            const SizedBox(height: 20),
-            Text('Farmacia Produto titulo 50g'),
-            const SizedBox(height: 15),
-            Row(
-              children: [
-                Text('R\$10,00'),
-                const SizedBox(width: 10),
-                IconButton(
-                  onPressed: () => {},
-                  icon: Icon(Icons.add_shopping_cart_rounded)
-                )
-              ],
+    child: GestureDetector(
+      onTap: onTap,
+      child: Container(
+        width: 130,
+        height: 185,
+        decoration: BoxDecoration(
+          color: customLightGrey,
+          boxShadow: <BoxShadow>[
+            BoxShadow(
+              color: const Color.fromARGB(32, 56, 56, 56),
+              blurRadius: 13.0,
+              offset: Offset(10, 6)
             )
           ],
+          borderRadius: BorderRadius.circular(10)
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(5),
+          child: Column(
+            children: [
+              Image.network(product['image']),
+              const SizedBox(height: 20),
+              Text(product['name']),
+              const SizedBox(height: 15),
+              Row(
+                children: [
+                  Text('R\$${price()}'),
+                  const SizedBox(width: 10),
+                  IconButton(
+                    onPressed: () => {},
+                    icon: Icon(Icons.add_shopping_cart_rounded)
+                  )
+                ],
+              )
+            ],
+          ),
         ),
       ),
-    ),
+    ) 
   );
 }
