dc2748e06f223929ccf95e77854ce88e0f6e1ded
Gabriel Frasato <gabriel.frasato@hotmail.com>
2025-05-31 09:35:51 -0300

    :sparkles: feat: Add a blue button to search

--- lib/widgets/custom_text_field.dart
+++ lib/widgets/custom_text_field.dart
@@ -1,3 +1,4 @@
+import 'package:ecommerce_app/constants/custom_colors.dart';
 import 'package:flutter/material.dart';
 
 class CustomTextField extends StatelessWidget{
@@ -6,25 +7,43 @@ class CustomTextField extends StatelessWidget{
   @override
   Widget build(BuildContext context) =>Padding(
     padding: EdgeInsets.symmetric(horizontal: 45),
-    child: TextField(
-      style: TextStyle(color: Colors.black),
-      decoration: InputDecoration(
-        isDense: true,
-        prefixIcon: Icon(Icons.search, size: 17),
-        prefixIconColor: Colors.black,
-        hintText: 'Ache o que precisa...',
-        hintStyle: TextStyle(fontWeight: FontWeight.bold, fontSize: 14),
-        fillColor: const Color.fromARGB(255, 233, 233, 233),
-        filled: true,
-        border: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(12),
-          borderSide: BorderSide.none
+    child: Row(
+      children: [
+        Expanded(
+          child: TextField(
+            style: TextStyle(color: customTextGrey),
+            decoration: InputDecoration(
+              isDense: true,
+              contentPadding: const EdgeInsets.symmetric(vertical: 12),
+              hintText: 'Ache o que precisa...',
+              hintStyle: TextStyle(fontSize: 14, color: customTextGrey),
+              fillColor: customLightGrey,
+              filled: true,
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(12),
+                borderSide: BorderSide.none
+              ),
+              errorStyle: TextStyle(
+                color: Colors.amber,
+                fontSize: 14
+              )
+            ),
+          )   
         ),
-        errorStyle: TextStyle(
-          color: Colors.amber,
-          fontSize: 14
+        const SizedBox(width: 10),
+        Container(
+          height: 45,
+          width: 45,
+          decoration: BoxDecoration(
+            color: customBlue,
+            borderRadius: BorderRadius.circular(12)
+          ),
+          child: IconButton(
+            onPressed: () => {},
+            icon: Icon(Icons.search_rounded, color: customLightGrey, size: 25,)
+          ),
         )
-      ),
+      ],
     )
   );
 }
