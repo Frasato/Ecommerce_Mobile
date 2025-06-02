a4e0887c47e0aa875a27b78b6886ccf5f008f156
Gabriel Frasato <gabriel.frasato@hotmail.com>
2025-06-02 10:27:25 -0300

    :lipstick: feat: Create a default style for text button

---
+++ lib/styles/textButtonStyle.dart
@@ -0,0 +1,9 @@
+import 'package:ecommerce_app/constants/custom_colors.dart';
+import 'package:flutter/material.dart';
+
+final ButtonStyle textButtonStyle = TextButton.styleFrom(
+  textStyle: const TextStyle(
+    color: customLightGrey,
+  ),
+  foregroundColor: customLightGrey
+);
