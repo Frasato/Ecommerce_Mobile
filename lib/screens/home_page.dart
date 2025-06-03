5c517a0352a9f011e4b64a546cbcfccc77548ff5
Gabriel Frasato <gabriel.frasato@hotmail.com>
2025-06-03 19:13:58 -0300

    :sparkles: feat: Add products category on home

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -3,6 +3,7 @@ import 'package:ecommerce_app/screens/product_page.dart';
 import 'package:ecommerce_app/services/product_service.dart';
 import 'package:ecommerce_app/widgets/custom_drawer_navigation.dart';
 import 'package:ecommerce_app/widgets/custom_text_field.dart';
+import 'package:ecommerce_app/widgets/footer.dart';
 import 'package:ecommerce_app/widgets/header.dart';
 import 'package:ecommerce_app/widgets/product_card.dart';
 import 'package:flutter/material.dart';
@@ -16,11 +17,15 @@ class HomePage extends StatefulWidget{
 
 class _HomePageState extends State<HomePage>{
   late Future<List<Map<String, dynamic>>> _productsFuture;
+  late Future<List<Map<String, dynamic>>> _productsCosmeticsFuture;
+  late Future<List<Map<String, dynamic>>> _productsFitnessFuture;
 
   @override
   void initState(){
     super.initState();
     _productsFuture = ProductService().getAllProducts();
+    _productsCosmeticsFuture = ProductService().getProductByCategory('Beleza');
+    _productsFitnessFuture = ProductService().getProductByCategory('Fit');
   }
 
   @override
@@ -78,7 +83,7 @@ class _HomePageState extends State<HomePage>{
                     final products = snapshot.data ?? [];
                     return ListView.builder(
                       scrollDirection: Axis.horizontal,
-                      itemCount: products.length,
+                      itemCount: products.length > 5? 5 : products.length,
                       itemBuilder: (context, index){
                         final product = products[index];
                         return ProductCard(
@@ -96,8 +101,101 @@ class _HomePageState extends State<HomePage>{
                   }
                 ) 
               ),
+              Container(
+                width: double.infinity,
+                padding: EdgeInsets.symmetric(horizontal: 45),
+                child: Text(
+                  'Produtos',
+                  style: TextStyle(fontWeight: FontWeight.w900, color: customBlue, fontSize: 18),
+                ),
+              ),
+              Container(
+                width: double.infinity,
+                padding: EdgeInsets.symmetric(horizontal: 45),
+                child: Text(
+                  'Cosméticos',
+                  style: TextStyle(fontWeight: FontWeight.w500, color: customTextGrey, fontSize: 14),
+                ),
+              ),
+              const SizedBox(height: 10),
+              SizedBox(
+                height: 250,
+                child: FutureBuilder<List<Map<String, dynamic>>>(
+                  future: _productsCosmeticsFuture,
+                  builder: (context, snapshot){
+                    if(snapshot.connectionState == ConnectionState.waiting){
+                      return Center(child: CircularProgressIndicator());
+                    }
+                    if(snapshot.hasError){
+                      return Center(child: Text('Erro ao carregar produtos'));
+                    }
+                    final products = snapshot.data ?? [];
+                    return ListView.builder(
+                      scrollDirection: Axis.horizontal,
+                      itemCount: products.length > 5? 5 : products.length,
+                      itemBuilder: (context, index){
+                        final product = products[index];
+                        return ProductCard(
+                          product: product,
+                          onTap: (){
+                            Navigator.push(
+                              context, MaterialPageRoute(
+                                builder: (_) => ProductPage(productId: product['id'], productCategory: product['category'],)
+                              )
+                            );
+                          }
+                        );
+                      }
+                    );
+                  }
+                ),    
+              ),
+              Container(
+                width: double.infinity,
+                padding: EdgeInsets.symmetric(horizontal: 45),
+                child: Text(
+                  'Fitness',
+                  style: TextStyle(fontWeight: FontWeight.w500, color: customTextGrey, fontSize: 14),
+                ),
+              ),
+              const SizedBox(height: 10),
+              SizedBox(
+                height: 250,
+                child: FutureBuilder<List<Map<String, dynamic>>>(
+                  future: _productsFitnessFuture,
+                  builder: (context, snapshot){
+                    if(snapshot.connectionState == ConnectionState.waiting){
+                      return Center(child: CircularProgressIndicator());
+                    }
+                    if(snapshot.hasError){
+                      return Center(child: Text('Erro ao carregar produtos'));
+                    }
+                    final products = snapshot.data ?? [];
+                    return ListView.builder(
+                      scrollDirection: Axis.horizontal,
+                      itemCount: products.length > 5? 5 : products.length,
+                      itemBuilder: (context, index){
+                        final product = products[index];
+                        return ProductCard(
+                          product: product,
+                          onTap: (){
+                            Navigator.push(
+                              context, MaterialPageRoute(
+                                builder: (_) => ProductPage(productId: product['id'], productCategory: product['category'],)
+                              )
+                            );
+                          }
+                        );
+                      }
+                    );
+                  }
+                ),
+                
+              ),
             ],
-          )
+          ),
+          SizedBox(height: 30),
+          Footer()
         ],
       ),
     ) 
