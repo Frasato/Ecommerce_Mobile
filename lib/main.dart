1591844cb6d6fb303b88f297e26a1f98720ac660
Gabriel Frasato <gabriel.frasato@hotmail.com>
2025-06-03 19:13:28 -0300

    :sparkles: feat; Add Register and Catalog route

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:ecommerce_app/screens/catalog_page.dart';
 import 'package:ecommerce_app/screens/home_page.dart';
 import 'package:ecommerce_app/screens/login_page.dart';
 import 'package:ecommerce_app/screens/register_page.dart';
@@ -23,7 +24,8 @@ class MyApp extends StatelessWidget {
       routes: {
         '/home': (context) => HomePage(),
         '/login': (context) => LoginPage(),
-        '/register': (context) => RegisterPage()
+        '/register': (context) => RegisterPage(),
+        '/catalog': (context) => CatalogPage(searchWord: '')
       },
     );
   }
