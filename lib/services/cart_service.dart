439fa93b2a82c9a2f3f27c8441a037788dde9fa7
Gabriel Frasato <gabriel.frasato@hotmail.com>
2025-06-04 22:46:27 -0300

    :sparkles: feat: Remove unnecessary parameters and create function to plus and minus itens

--- lib/services/cart_service.dart
+++ lib/services/cart_service.dart
@@ -6,7 +6,7 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 class CartService {
 
-  static Future<Map<String, dynamic>> takeCart(String userId) async{
+  static Future<Map<String, dynamic>> takeCart(String? userId) async{
     final prefs = await SharedPreferences.getInstance();
     String? token = prefs.getString('token');
 
@@ -28,16 +28,20 @@ class CartService {
     }
   }
 
-  static Future<List<dynamic>> getProductsOnCart(String userId) async{
+  static Future<List<Map<String, dynamic>>> getProductsOnCart() async{
+    final prefs = await SharedPreferences.getInstance();
+    final userId = prefs.getString('userId');
+
     final cartData = await takeCart(userId);
 
     List<dynamic> cartItems = cartData['cartItem'] ?? [];
 
-    List<Map<String, dynamic>> products = cartItems.map((item){
-      return Map<String, dynamic>.from(item['product']);
+    return cartItems.map((item) => {
+      'product': Map<String, dynamic>.from(item['product']),
+      'quantity': item['quantity'],
+      'cartItemId': item['id'],
     }).toList();
-
-    return products;
+    
   }
 
   static Future<void> removeItemFromCart(String cartItemId) async{
@@ -53,9 +57,12 @@ class CartService {
     );
   }
 
-  static Future<void> addItemOnCart(String userId, String productId) async{
+  static Future<void> addItemOnCart(String productId) async{
     final prefs = await SharedPreferences.getInstance();
     String? token = prefs.getString('token');
+    String? userId = prefs.getString('userId');
+
+    if(userId == null) throw Exception('User should not be null: $userId');
 
     await http.post(
       Uri.parse('$apiUrl/cart'),
@@ -81,5 +88,53 @@ class CartService {
         'Authorization':'Bearer $token'
       }
     );
+  }
+
+  static Future<double> getCartTotal() async{
+    final prefs = await SharedPreferences.getInstance();
+    final userId = prefs.getString('userId');
+
+    final cartData = await takeCart(userId);
+    final value = cartData['totalPrice'] ?? 0;
+    double finalValue = value / 100;
+    return finalValue;
+  }
+
+  static Future<void> plusOneCartItem(String cartItemId) async{
+    final prefs = await SharedPreferences.getInstance();
+    String? token = prefs.getString('token');
+
+    final response = await http.put(
+      Uri.parse('$apiUrl/cart/plus/$cartItemId'),
+      headers: {
+        'Content-Type':'application/json',
+        'Authorization':'Bearer $token'
+      }
+    );
+
+    if(response.statusCode == 200){
+      return;
+    }
+
+    throw Exception('Erro ao somar os itens no carrinho');
+  }
+
+  static Future<void> minusOneCartItem(String cartItemId) async{
+    final prefs = await SharedPreferences.getInstance();
+    String? token = prefs.getString('token');
+
+    final response = await http.put(
+      Uri.parse('$apiUrl/cart/minus/$cartItemId'),
+      headers: {
+        'Content-Type':'application/json',
+        'Authorization':'Bearer $token'
+      }
+    );
+
+    if(response.statusCode == 200){
+      return;
+    }
+
+    throw Exception('Erro ao somar os itens no carrinho');
   }
 }
