5ab54681cb3691d338dde6fd60be091a052e32aa
Gabriel Frasato <gabriel.frasato@hotmail.com>
2025-06-04 22:47:02 -0300

    :lipstick: feat: Style for Icon Button on Cart Item

---
+++ lib/styles/iconButtonStyle.dart
@@ -0,0 +1,8 @@
+import 'package:ecommerce_app/constants/custom_colors.dart';
+import 'package:flutter/material.dart';
+
+final ButtonStyle iconButtonStyle = IconButton.styleFrom(
+  elevation: 0,
+  foregroundColor: customLightGrey,
+  backgroundColor: customBlue
+);
