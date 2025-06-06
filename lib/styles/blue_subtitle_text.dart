3f54999ddf20b3cac3885a9e4f8bd88a932ac70b
Gabriel Frasato <gabriel.frasato@hotmail.com>
2025-06-06 11:46:01 -0300

    :sparkles: feat: Create text subtitle style

---
+++ lib/styles/blue_subtitle_text.dart
@@ -0,0 +1,8 @@
+import 'package:ecommerce_app/constants/custom_colors.dart';
+import 'package:flutter/widgets.dart';
+
+final TextStyle blueSubtitleTextStyle = TextStyle(
+  fontWeight: FontWeight.w900,
+  color: customBlue,
+  fontSize: 18
+);
