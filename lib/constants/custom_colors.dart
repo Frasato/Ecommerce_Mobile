0738afbe830ba80e6f59d609eb7ace0034c2bd7b
Gabriel Frasato <gabriel.frasato@hotmail.com>
2025-06-10 11:54:25 -0300

    :lipstick: feat: Add grey color

--- lib/constants/custom_colors.dart
+++ lib/constants/custom_colors.dart
@@ -5,3 +5,4 @@ const Color customYellow = Color.fromARGB(255, 254, 237, 0);
 const Color customRed = Color.fromARGB(255, 210, 14, 14);
 const Color customTextGrey = Color.fromARGB(255, 56, 56, 56);
 const Color customLightGrey = Color.fromARGB(255, 247, 247, 247);
+const Color customGrey = Color.fromARGB(255, 221, 221, 221);
